e94528133a41c3d40863e0a44787bc8ea04acf10
vagrant <vagrant@lighthouse-labs>
2016-02-04 03:13:10 +0000

    added REPL

--- popping_bottles.rb
+++ popping_bottles.rb
@@ -1,7 +1,9 @@
-def recycle(investment)
-  bottles = investment / 2
-  caps = investment / 4
-  return bottles + caps
+def recycle
+  puts "How much money are you going to spend?"
+  @investment = (gets.chomp).to_i
+  @bottles = @investment / 2
+  @caps = @investment / 4
+  puts @bottles + @caps
 end
 
-puts recycle(20)
+recycle
