b629c6b50c5f8f7339ef2abc83217975a5ba0166
vagrant <vagrant@lighthouse-labs>
2016-02-04 03:20:33 +0000

    added more functionality and fixed stupid math error

--- popping_bottles.rb
+++ popping_bottles.rb
@@ -1,9 +1,11 @@
 def recycle
   puts "How much money are you going to spend?"
   @investment = (gets.chomp).to_i
-  @bottles = @investment / 2
-  @caps = @investment / 4
-  puts @bottles + @caps
+  @pops = @investment / 2
+  @bottles = @pops / 2
+  @caps = @pops / 4
+  puts "You recieved #{@bottles + @caps} free pops for buying #{@pops} bottles
+  of pop.  #{@bottles} came from recycled bottles and #{@caps} came from caps." 
 end
 
 recycle
