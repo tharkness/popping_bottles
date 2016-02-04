d3b2788fb52322122f2f7c48c9abaf712d0c0026
vagrant <vagrant@lighthouse-labs>
2016-02-04 03:28:29 +0000

    added statement calculating remainder of bottles and caps.

--- popping_bottles.rb
+++ popping_bottles.rb
@@ -4,8 +4,9 @@ def recycle
   @pops = @investment / 2
   @bottles = @pops / 2
   @caps = @pops / 4
-  puts "You recieved #{@bottles + @caps} free pops for buying #{@pops} bottles
-  of pop.  #{@bottles} came from recycled bottles and #{@caps} came from caps." 
+  puts "You recieved #{@bottles + @caps} free pops for buying #{@pops} bottles of pop."  
+  puts "#{@bottles} came from recycled bottles and #{@caps} came from caps."
+  puts "You have #{@pops.divmod(2)[1]} bottles left, and #{@pops.divmod(4)[1]} caps leftover." 
 end
 
 recycle
